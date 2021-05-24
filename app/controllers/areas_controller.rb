d7b0058b76adc5896d6465cf6739ae0cb99369e9
koto338 <monmo4909@gmail.com>
2021-05-24 23:00:33 +0900

    Modal6

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -2,24 +2,19 @@ class AreasController < ApplicationController
 
  # テリ登録ボタン
   def new
-    @area = Area.all
     @area = Area.new
   end
 
-  def create
-    Area.create(area_params)
-    @area = Area.all
-  end  
-  # def create 
-  #   @area = Area.new(area_params)
-  #   if @area.save
-  #     flash[:success] = "登録しました"
-  #     redirect_to root_path 
-  #   else
-  #     flash.now[:danger] = "登録に失敗しました"
-  #     render new_area_path
-  #   end
-  # end
+  def create 
+    @area = Area.new(area_params)
+    if @area.save
+      flash[:success] = "登録しました"
+      redirect_to root_path 
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render area_new_path
+    end
+  end
 
   # 移動リンク
   def update
@@ -30,10 +25,8 @@ class AreasController < ApplicationController
 
 
   private
+
     def area_params
-      params.require(:area).permit(:town)
+      params.require(:area).permit(:ward, :town, :city_block, :status)
     end
-    # def area_params
-    #   params.require(:area).permit(:ward, :town, :city_block, :status)
-    # end
 end
