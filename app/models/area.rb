01ca28c87716b4a7d3d2a2f4737ae49d9f5631c8
koto338 <monmo4909@gmail.com>
2021-06-01 21:59:04 +0900

    Modal10

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,6 +1,7 @@
 class Area < ApplicationRecord
     has_many  :visit_histories
     validates :ward, presence: true
+    validates :town, presence: true
     validates :city_block, presence: true
     validates :status, presence: true
 end
