924de980c63d5dfe5e8d76c7e8f3ccf85c9ebb4c
koto338 <monmo4909@gmail.com>
2021-06-11 23:13:55 +0900

    訪問履歴3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
   def index
     @user = current_user
-    @areas = Area.all
-    # @visits = VisitHistory.find_by(user_id: current_user.id, area_id: area.id)
-    # order('id desc')
-
-
+    @visits = VisitHistory.where(user_id: current_user.id)
   end
 end
