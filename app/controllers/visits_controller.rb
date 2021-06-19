b42691707f0efead9fc1d89c24fdb64821bc1f66
koto338 <monmo4909@gmail.com>
2021-06-19 13:45:23 +0900

    訪問履歴6

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,24 +1,24 @@
 class VisitsController < ApplicationController
-  # 訪問履歴の登録
+  # 訪問一覧
   def show
     @area = Area.find(params[:id])
     @user = current_user
-    # @visit = VisitHistory.all
-    # @visit = current_user.visit_histories
+    @visits = VisitHistory.where(user_id: current_user.id, area_id: @area.id)
   end
-  
+  # 訪問履歴の登録
   def new
-    @visit = VisitHistory.new
     @area = Area.find(params[:id])
+    @visit = VisitHistory.new
   end
 
   def create
     @visit = VisitHistory.new(visit_params)
-    @area = Area.find(params[:id])
-    
+    @area =  current_user.areas
+    @areas = Area.find(params[:area_id])
+
     if @visit.save
       flash[:success] = "登録しました"
-      redirect_to visit_path
+      redirect_to visit_path(@areas)
     else
       flash[:danger] = "登録に失敗しました"
       render :new
@@ -28,6 +28,6 @@ class VisitsController < ApplicationController
   private
 
   def visit_params
-    params.permit(:customer, :appointment, :visit, :contract, :note).merge(user_id: current_user.id)
+    params.require(:visit_history).permit(:customer, :appointment, :visit, :contract, :note).merge(user_id: current_user.id, area_id: params[:area_id])
   end
 end
